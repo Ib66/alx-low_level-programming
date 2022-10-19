b701e855649b4a33973e52659e6e3bae16bcb1c2
Ibrahim <arowosayeibrahim2019@gmail.com>
2022-10-19 06:15:49 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  *print_alphabet - prints the letters of alphabet in a lowercase
+ *     * Return: void
+ *      */
+void print_alphabet(void)
+{
+		char count = 'a';
+
+			for (count = 'a'; count < '{'; count++)
+					{
+								_putchar(count);
+									}
+				_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this directory contains executable programs for C -functions, nested loops.
