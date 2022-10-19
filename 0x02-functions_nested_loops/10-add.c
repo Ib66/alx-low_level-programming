c3d7e7e97cef2877c2d940afdd60ffe029a9b66b
Ibrahim <arowosayeibrahim2019@gmail.com>
2022-10-19 06:21:52 -0700

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  * add - outputs the result of addition of two integers
+ *   * a: first parameter/integer for the function
+ *    * b: seconf parameter/integer for the function
+ *     *
+ *      * Description: prints the sum of two integer numbers
+ *       * Return: value of the addition is returned for success
+ *        */
+int add(int a, int b)
+{
+		int sum;
+
+			sum = a + b;
+				return (sum);
+}
