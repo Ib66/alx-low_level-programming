7d75356e72c41513aeb79942147ee577b39e07df
Ibrahim <arowosayeibrahim2019@gmail.com>
2022-10-20 17:37:25 -0700

    mistake

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  * _isupper - checks for uppercase character
+ *   * @c: integer parametr to be checked
+ *    *
+ *     * Description: checks for case sensitivity of inputted character
+ *      * Return: 1 if the inputted character is uppercase, else 0
+ *       */
+int _isupper(int c)
+
+{
+		if (c >= 65 && c <= 90)
+					return (1);
+			else
+						return (0);
+}
